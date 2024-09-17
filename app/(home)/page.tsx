01f3b720caa886167b89376df354f2a032248d23
tarifalhasan <tarifalhasan999@gmail.com>
2024-09-17 15:49:34 +0600

    Section4

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Section1 from "./_components/Section1";
 import Section2 from "./_components/Section2";
 import Section3 from "./_components/Section3";
+import Section4 from "./_components/Section4";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </>
   );
 }
