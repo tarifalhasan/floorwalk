0765900c3013cf5b55257b8ad4d63208e54edda7
tarifalhasan <tarifalhasan999@gmail.com>
2024-10-01 13:28:14 +0600

    Visual Merchandising Audit

--- app/visual-merchandising-audit/page.tsx
+++ app/visual-merchandising-audit/page.tsx
@@ -11,7 +11,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 const VisualMerchandisingAudit = () => {
-  const [activeTab, setActiveTab] = useState(10);
+  const [activeTab, setActiveTab] = useState(101);
   const TABS = [
     {
       name: "Promotion Audit",
